421c46006ff42a2842630e3e6c98db67810f54b2
rajasekharmekala <rajasekharreddymekala732@gmail.com>
2022-02-14 18:41:13 -0800

    NER added for keywords

--- server/keyword_builder.py
+++ server/keyword_builder.py
@@ -1,30 +1,41 @@
 from nltk.tokenize import TweetTokenizer
+# nltk.download()
 # nltk.download('stopwords')
-# from wordcloud import WordCloud, ImageColorGenerator
-# from PIL import Image
-# import matplotlib.pyplot as plt
-# import csv
 
 from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk import ne_chunk, tree
 
 class KeywordBuilder():
 
     @staticmethod
     def get_keyword_counts(tweets, maxwords=500):
-        tokenizer = TweetTokenizer()
         dict = {}
         for id, created_at, text  in tweets:
-            for token in tokenizer.tokenize(text.lower()):
+            tokenizer = TweetTokenizer()
+            tokens = tokenizer.tokenize(text)
+            pos_tags = pos_tag(tokens)
+            tagged_tokens = [' '.join(KeywordBuilder.concat_leaves(ne.leaves()) ) for ne in ne_chunk(pos_tags, binary=True) if isinstance(ne, tree.Tree)]
+            for token in tagged_tokens:
+                token = token.lower()
                 dict[token] = dict.get(token, 0)+1
         items = sorted(dict.items(), key=lambda kv: kv[1], reverse=True)
         count = 0
         stop_words = set(stopwords.words('english'))
         result = []
         for key, value in items:
-            if key  in stop_words or len(key)<2:
+            if key  in stop_words or len(key)<=2:
                 continue
             count+=1
             if(count>maxwords):
                 break
             result.append({"text": key, "value": value})
         return result
+
+    @staticmethod
+    def is_proper_noun(pos):
+        return pos == 'NNP'
+
+    @staticmethod
+    def concat_leaves(leaves):
+        return list(x[0] for x in filter(lambda x: KeywordBuilder.is_proper_noun(x[1]),leaves))
